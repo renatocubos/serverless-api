e519633c8fcab06ea6e7b1524cdd794e2ecc79e6
Renato Macêdo <renato.neto@tc.com.br>
2021-11-22 19:21:27 -0300

    fix: change update note function to use right path parameter

--- notes/src/update.ts
+++ notes/src/update.ts
@@ -3,13 +3,13 @@ import { createHandler, response } from "helpers";
 import { validateBody } from "./validate";
 
 export const handler: APIGatewayProxyHandler = createHandler(async (event) => {
-  const id = event.pathParameters?.id;
+  const id = event.pathParameters?.noteID;
 
   if (!id) {
-    return response(400, { message: "ID Not Found" });
+    return response(400, { message: event.pathParameters });
   }
 
   const body = await validateBody(event.body);
 
-  return response(200, { id, body, updatedAt: new Date() });
+  return response(200, { id, ...body, updatedAt: new Date() });
 });
