7140b4ab63794a4023103cf1d2f150d96cd1dc32
Renato Macêdo <renato.neto@tc.com.br>
2021-11-25 00:01:18 -0300

    chore:  fix lambda package

--- packages/notes/src/get.ts
+++ packages/notes/src/get.ts
@@ -1,35 +1,24 @@
-import { Lambda } from "aws-sdk";
 import { createHandler, response, database as db } from "helpers";
-const lambda = new Lambda();
-import { addNumbers } from "math";
 
-const invokeLambda = async (functionName: string, payload?: unknown) => {
-  try {
-    const response = await lambda
-      .invoke({
-        FunctionName: functionName,
-        InvocationType: "RequestResponse",
-        Payload: JSON.stringify(payload),
-      })
-      .promise();
+// const invokeLambda = async (functionName: string, payload?: unknown) => {
+//   try {
+//     const response = await lambda
+//       .invoke({
+//         FunctionName: functionName,
+//         InvocationType: "RequestResponse",
+//         Payload: JSON.stringify(payload),
+//       })
+//       .promise();
 
-    return JSON.parse(response.Payload as string);
-  } catch (e) {
-    console.log("error", e);
-    return Error(JSON.stringify(e));
-  }
-};
+//     return JSON.parse(response.Payload as string);
+//   } catch (e) {
+//     console.log("error", e);
+//     return Error(JSON.stringify(e));
+//   }
+// };
 
 export const handler = createHandler(async () => {
-  const res = await invokeLambda(`math-api-${process.env.STAGE ?? "dev"}-sum`, {
-    pathParameters: { a: addNumbers(2, 3, 3, 3), b: addNumbers(3, 3, 3, 3) },
-  });
-
   const notes = await db.prisma.note.findMany();
 
-  return response(200, {
-    operation: "get",
-    sum: JSON.parse(res.body).result,
-    notes,
-  });
+  return response(200, notes);
 });
